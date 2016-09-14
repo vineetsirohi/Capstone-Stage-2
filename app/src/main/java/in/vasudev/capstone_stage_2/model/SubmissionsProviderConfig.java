19d03014e112e819fd7f6f068ffd40b98d957d83
Vasudev <vineetksirohi@gmail.com>
2016-09-14 14:12:57 +0530

    Removed bug with content provider authority that prevented free and paid versions simaultaneous installation

--- app/src/main/java/in/vasudev/capstone_stage_2/model/SubmissionsProviderConfig.java
+++ app/src/main/java/in/vasudev/capstone_stage_2/model/SubmissionsProviderConfig.java
@@ -3,13 +3,14 @@ package in.vasudev.capstone_stage_2.model;
 import ckm.simple.sql_provider.UpgradeScript;
 import ckm.simple.sql_provider.annotation.ProviderConfig;
 import ckm.simple.sql_provider.annotation.SimpleSQLConfig;
+import in.vasudev.capstone_stage_2.BuildConfig;
 
 /**
  * Created by vineet on 11-Sep-16.
  */
 @SimpleSQLConfig(
         name = "SubmissionsProvider",
-        authority = "in.vasudev.capstone_stage_2.submissions_provider.authority",
+        authority = BuildConfig.APPLICATION_ID + ".submissions_provider.authority",
         database = "database.db",
         version = 1)
 public class SubmissionsProviderConfig implements ProviderConfig {
